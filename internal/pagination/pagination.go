eba48666597b2f0ca6abb9f21824c851765c6668
César Escudero <cedaesca@gmail.com>
2026-06-26 23:34:10 -0400

    feat: search metadata (#21)

--- internal/pagination/pagination.go
+++ internal/pagination/pagination.go
@@ -19,11 +19,12 @@ func (f Filters) Offset() int {
 }
 
 type Metadata struct {
-	CurrentPage  int `json:"current_page,omitempty"`
-	PageSize     int `json:"page_size,omitempty"`
-	FirstPage    int `json:"first_page,omitempty"`
-	LastPage     int `json:"last_page,omitempty"`
-	TotalRecords int `json:"total_records,omitempty"`
+	CurrentPage  int  `json:"current_page,omitempty"`
+	PageSize     int  `json:"page_size,omitempty"`
+	FirstPage    int  `json:"first_page,omitempty"`
+	LastPage     int  `json:"last_page,omitempty"`
+	TotalRecords int  `json:"total_records,omitempty"`
+	SearchTimeMs *int `json:"search_time_ms,omitempty"`
 }
 
 func CalculateMetadata(totalRecords, page, pageSize int) Metadata {
